1b4150b17e6fc742b8d7a6e2603d51c3578d2146
Prelang Builder <builder@prelang.com>
2014-12-18 06:43:05 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/restaurant_feedbacks_controller.rb
+++ app/controllers/restaurant_feedbacks_controller.rb
@@ -1,4 +1,8 @@
 class RestaurantFeedbacksController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_restaurant_feedback, only: [:show, :edit, :update, :destroy]
 
   # GET /restaurant_feedbacks
