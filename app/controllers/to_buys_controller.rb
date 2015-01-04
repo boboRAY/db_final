689322f921da60168c2749852acdccf8ba046907
Chih-Wei Chang <jrweizhang@gmail.com>
2015-01-04 15:28:27 +0800

    Fix permission.

--- app/controllers/to_buys_controller.rb
+++ app/controllers/to_buys_controller.rb
@@ -1,7 +1,7 @@
 class ToBuysController < ApplicationController
 
   #->Prelang (scaffolding:rails/scope_to_user)
-  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+  before_filter :require_user_signed_in
 
   before_action :set_to_buy, only: [:show, :edit, :update, :destroy]
 
